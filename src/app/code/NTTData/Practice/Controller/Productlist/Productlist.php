23f796a3f268528ce99faa5493d2d6ae5cdcef7e
Norman <norclaro@emeal.nttdata.com>
2022-11-22 20:50:30 -0300

    now storeManager instantiate from constructor

--- src/app/code/NTTData/Practice/Controller/Productlist/Productlist.php
+++ src/app/code/NTTData/Practice/Controller/Productlist/Productlist.php
@@ -4,27 +4,28 @@ namespace NTTData\Practice\Controller\Productlist;
 class Productlist extends \Magento\Framework\App\Action\Action
 {
 	protected $_pageFactory;
+	private $storeManager;
 
 	public function __construct(
 		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
+		\Magento\Framework\View\Result\PageFactory $pageFactory,
+		\Magento\Store\Model\StoreManagerInterface $storeManager)
 	{
+		$this->storeManager = $storeManager;
 		$this->_pageFactory = $pageFactory;
 		return parent::__construct($context);
 	}
 
 	public function execute()
 	{
-		$objectManager =  \Magento\Framework\App\ObjectManager::getInstance();
-		$storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
-		$date = $storeManager->getStore()->getStoreId();
+		$storeId = $this->storeManager->getStore()->getId();
         $pageFactory = $this->_pageFactory->create();
-        $date = 1;
-		if ($date != 1){
+        $storeId = 1;
+		if ($storeId != 1){
     		date_default_timezone_set("America/Argentina/Buenos_Aires");
 		}
-		$time = date('H:i:s');
-		$pageFactory->getConfig()->getTitle()->set(__('Now being %1, I am learning translations', $time));
+		$timeFormat = date('H:i:s');
+		$pageFactory->getConfig()->getTitle()->set(__('Now being %1, I am learning translations', $timeFormat));
 		return $this->_pageFactory->create();
 
     }
